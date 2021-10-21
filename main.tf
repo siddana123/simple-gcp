dde8a65d67072ca397154c7ed2706f0615ae61e1
siddu <goudasiddu@gmail.com>
2021-10-21 22:33:15 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,31 @@
+provider "google" {
+ //credentials = file("CREDENTIALS_FILE.json")
+ project     = "unique-balancer-327214"
+ region      = "us-west1"
+}
+// Terraform plugin for creating random ids
+resource "random_id" "instance_id" {
+ byte_length = 8
+}
+
+// A single Compute Engine instance
+resource "google_compute_instance" "default" {
+ name         = "flask-vm-${random_id.instance_id.hex}"
+ machine_type = "f1-micro"
+ zone         = "us-west1-a"
+
+ boot_disk {
+   initialize_params {
+     image = "debian-cloud/debian-9"
+   }
+ }
+
+
+ network_interface {
+   network = "default"
+
+   access_config {
+     // Include this section to give the VM an external ip address
+   }
+ }
+}
